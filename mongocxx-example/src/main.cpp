b61e8b4b3b68bedae173a5906328773db725dfae
Andrés Martínez Vargas <49073466+vmartinezandres@users.noreply.github.com>
2021-04-19 19:28:27 -0600

    Un apiRest funcional

--- mongocxx-example/src/main.cpp
+++ mongocxx-example/src/main.cpp
@@ -1,19 +1,11 @@
-#include <iostream>
 
-#include "mongocxx/instance.hpp"
-#include "mongodb_handler.h"
+#include "served/multiplexer.hpp"
+#include "http_server.h"
 
-void print_result(const bool &result, const char* operation) {
-    std::cout << "the " << operation << (result ? " worked." : " did't work.") << std::endl;
-}
 int main() {
-    mongocxx::instance instance;
-    learning::MongoDbHandler mhandler;
-    // mhandler.AddCharacterToDb("Donkey Kong", "medium", 0);
-    // mhandler.AddCharacterToDb("Mario", "small", 0);
+    served::multiplexer multiplexer;
+    learning::HttpServer http_server(multiplexer);
 
-    // bool result = mhandler.UpdateWins("607cc7da2731172d3e3a05c7");
-    bool result = mhandler.RemoveCharacterFromDb("607cc63bb690a11c73711c42");
-    print_result(result, "remove");
-    std::cout << "Hello Easy C++ project!" << std::endl;
+    http_server.InitializeEndpoints();
+    http_server.StartServer();
 }
